41da2ab16f18279523a5ef6831e37aa1dec67ad1
Иван <ivmedv5@gmail.com>
2023-07-03 13:39:18 +0700

    Pt_2_3: Full task

---
+++ Блок 2/3.py
@@ -0,0 +1,17 @@
+def check(string):
+    """Проверяет, чтобы введенная строка была числом"""
+    while not string.isnumeric():
+        print("Введите должна быть числом!")
+        string = input()
+    return string
+
+
+num = ""
+num = check(num)
+sum = 0
+for i in range(len(num)):
+    sum += int(num[i])**len(num)
+if sum == int(num):
+    print("Является числом Армстронга")
+else:
+    print("Не является числом Армстронга")
