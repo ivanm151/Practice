b62932ab6322669c31e5f9017515a8421cbf6edf
Иван <ivmedv5@gmail.com>
2023-07-03 13:40:25 +0700

    Pt_2_9: Full task

---
+++ Блок 2/9.py
@@ -0,0 +1,16 @@
+def check(string):
+    """Проверяет, чтобы введенная строка была числом"""
+    while not string.lstrip("-").isnumeric():
+        print("Введите число!")
+        string = input()
+    return string
+
+
+a = str(11)
+while len(a) != len(set(a)):
+    a = input("Введите число, все цифры которого различны:\n")
+    a = check(a)
+m = list(a)
+print("Порядковый номер максимальной цифры с начала списка:", m.index(max(m))+1, "С конца:", len(m)-m.index(max(m)))
+
+
