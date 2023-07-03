74dc2c0e8667e981c6b0146e082b66148329fa0b
Иван <ivmedv5@gmail.com>
2023-07-03 13:39:08 +0700

    Pt_2_2: Full task

---
+++ Блок 2/2.py
@@ -0,0 +1,9 @@
+progs = ["Новости", "Прогноз погоды", "Кино", "Охота и рыбалка"]
+for i in progs:
+    print(i)
+prog = input("Введите название передачи: ")
+num = int(input("Введите порядковый номер передачи в списке: "))-1
+progs.insert(num, prog)
+print("Новый список:")
+for i in progs:
+    print(i)
