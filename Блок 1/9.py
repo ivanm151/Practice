c817cda6f352947a7a9467764d13daa172a44df0
Иван <ivmedv5@gmail.com>
2023-07-07 01:06:50 +0700

    Pt_1_9: Full task

---
+++ Блок 1/9.py
@@ -0,0 +1,11 @@
+def check(string):
+    """Проверяет, чтобы введенная строка была числом"""
+    while not string.lstrip("-").isnumeric():
+        print("Строка должна быть числом!")
+        string = input("Введите число: ")
+    return int(string)
+
+
+a, b, = "", ""
+a, b = check(a), check(b)
+print("Произведение чисел", a, "и", b, "равно", a*b)
