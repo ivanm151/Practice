6e401feb0fa87caf129b154827c230af74b5757b
Иван <ivmedv5@gmail.com>
2023-07-07 01:06:45 +0700

    Pt_1_8: Full task

---
+++ Блок 1/8.py
@@ -0,0 +1,20 @@
+def check_name(string):
+    """Проверяет, чтобы введенная строка состояла только из букв"""
+    while not string.isalpha():
+        print("Строка должна содержать только буквы!")
+        string = input("Введите имя: ")
+    return string
+
+
+def check_age(string):
+    """Проверяет, чтобы введенная строка была числом"""
+    while not string.isnumeric():
+        print("Строка должна быть числом!")
+        string = input("Введите возраст: ")
+    return int(string)
+
+
+name, age = "", ""
+name = check_name(name)
+age = check_age(age)
+print("Привет,", name+"! Тебе уже", age, "лет.")
