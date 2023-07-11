14f917efbf23a7b8b044aa002bd2989d465d6db2
Иван <ivmedv5@gmail.com>
2023-07-12 00:55:47 +0700

    Pt_4_3: Full task

---
+++ Блок 4/3.py
@@ -0,0 +1,12 @@
+def simple(number):
+    """Проверяет, является ли число простым"""
+    for i in range(2, round(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+m = int(input("С какого числа проверять: "))
+n = int(input("До какого числа проверять: "))
+print(*[i for i in range(m, n + 1) if simple(i)], sep=', ')
+# Создание и распаковка списка перестановок заданного списка чисел, перестановки разделены запятыми
