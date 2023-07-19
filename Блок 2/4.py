a3dd2756d27e30584bce337bcc50ad37c766a7bd
ivanm151 <137757338+ivanm151@users.noreply.github.com>
2023-07-19 14:48:53 +0700

    Update 4.py

--- Блок 2/4.py
+++ Блок 2/4.py
@@ -6,8 +6,7 @@ D = b ** 2 - 4 * a * c
 if D < 0:
     print("Корней нет")
 elif D == 0:
-    print("x =", -b/(2 * a))
+    print("x =", -b / (2 * a))
 else:
     print("x1 =", (-b + D ** 0.5) / (2 * a))
     print("x2 =", (-b - D ** 0.5) / (2 * a))
-    
