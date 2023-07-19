c2c27d9c8b0bed2470508311766764d058670ce7
ivanm151 <137757338+ivanm151@users.noreply.github.com>
2023-07-19 14:40:54 +0700

    Update 4.py

--- Блок 2/4.py
+++ Блок 2/4.py
@@ -1,12 +1,13 @@
 a = int(input("Введите коэф. А: "))
 b = int(input("Введите коэф. В: "))
 c = int(input("Введите коэф. С: "))
-print(str(a)+"x^2 +", str(b)+"x +", str(c), "= 0")
-D = b**2 - 4*a*c
+print(str(a) + "x^2 +", str(b) + "x +", str(c), "= 0")
+D = b ** 2 - 4 * a * c
 if D < 0:
     print("Корней нет")
 elif D == 0:
-    print("x =", -b/(2*a))
+    print("x =", -b/(2 * a))
 else:
     print("x1 =", (-b + D ** 0.5) / (2 * a))
     print("x2 =", (-b - D ** 0.5) / (2 * a))
+    
